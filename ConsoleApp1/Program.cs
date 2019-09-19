672c3e5260db2a2e6a3af50aa199748dde431fa2
鲁俊 <lujunasd@sina.cn>
2019-09-19 11:26:43 +0800

    测试

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -119,14 +119,14 @@ namespace ConsoleApp1
             //Console.ReadLine();
             #endregion
 
-            //Cat.sid = 100;
-            //Cat mimi = new Cat("mimi");
-            //Cat pipi = new Cat("pipi");
-            //bool bol = true;
-            //Int32 i = new Int32();
-            //int j = 1;
-            //int k = bol ? i : j;
-            //Console.WriteLine(k);
+            Cat.sid = 100;
+            Cat mimi = new Cat("mimi");
+            Cat pipi = new Cat("pipi");
+            bool bol = true;
+            Int32 i = new Int32();
+            int j = 1;
+            int k = bol ? i : j;
+            Console.WriteLine(k);
 
             Console.ReadLine();
         }
